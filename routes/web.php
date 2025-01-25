dbbe17fae54a245f67b2b6d03f54110013cb73a1
Lucas Barbosa <corplaxus@gmail.com>
2025-01-25 18:25:49 -0300

    criando primeiro controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/users', [ UserController::class, 'index' ]);
 
 Route::get('/', function () {
     return view('welcome');
