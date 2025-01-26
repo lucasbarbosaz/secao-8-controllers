8027f6976c1f1c42834a2f2e9f981990c561c54f
Lucas Barbosa <corplaxus@gmail.com>
2025-01-26 15:34:01 -0300

    metodos na rota

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,9 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [ UserController::class, 'index' ]);
 
-Route::get('/users/{user}', [ UserController::class, 'show' ]);
-
+//Route::resource('users', UserController::class)->except(['store', 'destroy']); //exceto store e destroy
+Route::resource('users', UserController::class)->only(['index', 'show']); //apenas index e show
 // Route::get('/', function () {
 //     return view('welcome');
 // });
